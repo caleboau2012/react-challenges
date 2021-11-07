b8e868afce1b0aed66c945b9490b0a12b9f14d2f
Mbakwe Caleb <caleboau2012@gmail.com>
2021-11-07 20:25:42 +0000

    Using functional components challenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+// import react from 'react';
+
+const SideBar = () => (
+<div>
+    <a href="">My Photos</a>
+    <a href="">My Illustrations</a>
+    <a href="">My Paintings</a>
+</div>
+)
+
+export default SideBar
