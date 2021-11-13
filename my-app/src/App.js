c13c6a86424392af9b8689f0abb6a1a0caaeda4c
Mbakwe Caleb <caleboau2012@gmail.com>
2021-11-13 15:45:58 +0000

    Using Class Components Challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import css from './App.module.css';
+import NavBarSimple from './components/NavBarSimple';
 import SideBar from './components/Sidebar';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <NavBarSimple />
       <SideBar />
     </div>
   );
---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import css from './css/NavBarSimple.module.css'
+
+class NavBarSimple extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "F. U.",
+            loggedIn: false,
+            action: "log in"
+        };
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => ({
+            message: prevState.loggedIn ? "F. U." : "Welcome, user",
+            action: prevState.loggedIn ? "log in": "log out",
+            loggedIn: !prevState.loggedIn
+        }));
+    }
+
+    render() {
+        return (
+            <div className = {css.NavBar}>
+                <h1></h1>
+                <span>{this.state.message}</span>
+                <button onClick = {() => this.handleClick()}>{this.state.action}</button>
+            </div>
+        )
+    }
+}
+
+export default NavBarSimple
