f4f170b6d584cf581b7c2ab0433db2c5bce2bbd8
Mbakwe Caleb <caleboau2012@gmail.com>
2021-11-14 16:58:12 +0000

    Component Lifecycles Challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import {savedPosts} from '../posts.json'
-import css from './css/Content.module.css'
-import PostItem from './PostItem'
+import React from "react";
+import { savedPosts } from "../posts.json";
+import css from "./css/Content.module.css";
+import Loader from "./Loader";
+import PostItem from "./PostItem";
 
 class Content extends React.Component {
-    constructor(props) {
-        super(props)
-        console.log(savedPosts)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <div className={css.TitleBar}>
-                    <h1>My Posts</h1>
-                </div>
-                <div className={css.SearchResults}>
-                    <PostItem posts={savedPosts} />
-                </div>
-            </div>
-        )
-    }
+  componentDidMount() {
+    setTimeout(() => this.setState({ isLoaded: true }), 2000);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className={css.TitleBar}>
+          <h1>My Posts</h1>
+        </div>
+        <div className={css.SearchResults}>
+          {this.state.isLoaded ? <PostItem posts={savedPosts} /> : <Loader />}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Content
+export default Content;
---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,9 @@
+import css from "./css/Loader.module.css"
+
+const Loader = (props) => (
+    <div className={css.Loader}>
+        
+    </div>
+)
+
+export default Loader
