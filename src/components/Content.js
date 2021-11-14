622f6fc03209212646a1d611f994c9f4ff509a4e
Mbakwe Caleb <caleboau2012@gmail.com>
2021-11-14 18:44:10 +0000

    Search Feature Challenge

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,21 +9,53 @@ class Content extends React.Component {
     super(props);
     this.state = {
       isLoaded: false,
+      posts: savedPosts,
     };
   }
 
   componentDidMount() {
-    setTimeout(() => this.setState({ isLoaded: true }), 2000);
+    this.setState({
+      isLoaded: true,
+    });
   }
+
+  handleSearch = (event) => {
+    this.setState({
+      isLoaded: false,
+    });
+    let searchText = event.target.value.toLowerCase();
+    let filteredPosts = savedPosts.filter(
+      (post) =>
+        post.name.toLowerCase().includes(searchText) ||
+        post.title.toLowerCase().includes(searchText)
+    );
+    this.setState({
+      posts: filteredPosts,
+      isLoaded: true,
+    });
+  };
 
   render() {
     return (
       <div>
         <div className={css.TitleBar}>
           <h1>My Posts</h1>
+          <form>
+            <label htmlFor="searchInput">Search:</label>
+            <input
+              onChange={this.handleSearch}
+              type="search"
+              id="searchInput"
+            />
+            <h4>Posts found: {this.state.posts.length}</h4>
+          </form>
         </div>
         <div className={css.SearchResults}>
-          {this.state.isLoaded ? <PostItem posts={savedPosts} /> : <Loader />}
+          {this.state.isLoaded ? (
+            <PostItem posts={this.state.posts} />
+          ) : (
+            <Loader />
+          )}
         </div>
       </div>
     );
